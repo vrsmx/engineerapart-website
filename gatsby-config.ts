fd8f3fda75f3243380718939cd1ae0ecd6fa23fe
vrsmx <vrsmx.master@gmail.com>
2022-11-25 00:24:24 -0600

    remove unnecesary flag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,9 +7,6 @@ const config: GatsbyConfig = {
     title: `engineerapart-website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  flags: {
-    DEV_SSR: true,
-  },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
