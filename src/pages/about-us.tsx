72f3facf23bc72e593cf7959e5c6746983b59914
vrsmx <vrsmx.master@gmail.com>
2022-12-02 02:06:04 -0600

    about page update

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import {HeadFC, PageProps} from 'gatsby';
-import {Topbar} from 'src/components/Topbar/Topbar';
-import {Footer} from 'src/components/Footer/Footer';
-import {PageHeaderNavbar} from 'src/components/Navbar/PageHeaderNavbar/PageHeaderNavbar';
 
 const AboutPage: React.FC<PageProps> = () => {
   return (
     <>
-      <Topbar />
-      <main className="page">
-        <PageHeaderNavbar />
-      </main>
-      <Footer />
+      <main className="page"></main>
     </>
   );
 };
