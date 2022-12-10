81a86ed98ff692711ad09eb8aa38cfbf668e4fb6
vrsmx <vrsmx.master@gmail.com>
2022-12-09 21:09:52 -0600

    Fix logo gatsby class clashing

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -10,48 +10,48 @@ export const Logo: React.FC<Props> = ({light = false}) => {
   return (
     <div className="logo-component">
       <Link to="/">
-        {light ? (
-          <StaticImage
-            src={'../../images/full-white.png'}
-            className={styles.full_logo}
-            alt="EngineerApart Logo"
-            width={200}
-            aspectRatio={196 / 23}
-            transformOptions={{fit: 'contain', cropFocus: 'attention'}}
-            placeholder="blurred"
-          />
-        ) : (
-          <StaticImage
-            className={styles.full_logo}
-            src={'../../images/full-gradient.png'}
-            alt="EngineerApart Logo"
-            width={200}
-            aspectRatio={196 / 23}
-            transformOptions={{fit: 'contain', cropFocus: 'attention'}}
-            placeholder="blurred"
-          />
-        )}
-        {light ? (
-          <StaticImage
-            className={styles.icon_logo}
-            src={'../../images/icon-white.png'}
-            alt="EngineerApart Logo"
-            width={50}
-            aspectRatio={100 / 44}
-            transformOptions={{fit: 'contain', cropFocus: 'attention'}}
-            placeholder="blurred"
-          />
-        ) : (
-          <StaticImage
-            className={styles.icon_logo}
-            src={'../../images/icon-gradient.png'}
-            alt="EngineerApart Logo"
-            width={50}
-            aspectRatio={100 / 44}
-            transformOptions={{fit: 'contain', cropFocus: 'attention'}}
-            placeholder="blurred"
-          />
-        )}
+        <div className={styles.full_logo}>
+          {light ? (
+            <StaticImage
+              src={'../../images/full-white.png'}
+              alt="EngineerApart Logo"
+              width={200}
+              aspectRatio={196 / 23}
+              transformOptions={{fit: 'contain', cropFocus: 'attention'}}
+              placeholder="blurred"
+            />
+          ) : (
+            <StaticImage
+              src={'../../images/full-gradient.png'}
+              alt="EngineerApart Logo"
+              width={200}
+              aspectRatio={196 / 23}
+              transformOptions={{fit: 'contain', cropFocus: 'attention'}}
+              placeholder="blurred"
+            />
+          )}
+        </div>
+        <div className={styles.icon_logo}>
+          {light ? (
+            <StaticImage
+              src={'../../images/icon-white.png'}
+              alt="EngineerApart Logo"
+              width={50}
+              aspectRatio={100 / 44}
+              transformOptions={{fit: 'contain', cropFocus: 'attention'}}
+              placeholder="blurred"
+            />
+          ) : (
+            <StaticImage
+              src={'../../images/icon-gradient.png'}
+              alt="EngineerApart Logo"
+              width={50}
+              aspectRatio={100 / 44}
+              transformOptions={{fit: 'contain', cropFocus: 'attention'}}
+              placeholder="blurred"
+            />
+          )}
+        </div>
       </Link>
     </div>
   );
