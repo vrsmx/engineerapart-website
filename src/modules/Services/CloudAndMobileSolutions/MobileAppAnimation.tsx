4f2edb5d8c1cfd97c948fbf1294f8b877094c4ea
vrsmx <vrsmx.master@gmail.com>
2023-01-10 23:19:19 -0600

    Animate mobile view

--- src/modules/Services/CloudAndMobileSolutions/MobileAppAnimation.tsx
+++ src/modules/Services/CloudAndMobileSolutions/MobileAppAnimation.tsx
@@ -1,35 +1,133 @@
 import * as React from 'react';
+import {motion} from 'framer-motion';
 import * as styles from './CloudAndMobileSolutions.module.scss';
+
+const uiContainerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      ease: 'linear',
+      duration: 0.25,
+      delay: 0.5,
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const uiFieldVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'afterChildren',
+      ease: 'linear',
+      duration: 0.25,
+    },
+  },
+};
+
+const notificationsContainerVariants = {
+  hidden: {
+    y: -200,
+  },
+  visible: {
+    y: 0,
+    transition: {
+      when: 'afterChildren',
+      ease: 'linear',
+      duration: 0.25,
+    },
+  },
+};
+
+const navbarContainerVariants = {
+  hidden: {
+    y: 200,
+  },
+  visible: {
+    y: 0,
+    transition: {
+      when: 'afterChildren',
+      ease: 'linear',
+      duration: 0.25,
+    },
+  },
+};
+
+const carrouselVariants = {
+  hidden: {
+    x: 300,
+  },
+  visible: {
+    x: 0,
+    transition: {
+      when: 'afterChildren',
+      ease: 'easeOut',
+      duration: 0.5,
+    },
+  },
+};
 
 const Carrousel: React.FC = () => {
   return (
-    <div className={styles.mobile_animation_carrousel}>
+    <motion.div
+      className={styles.mobile_animation_carrousel}
+      variants={carrouselVariants}
+    >
       <div className={styles.mobile_animation_carrousel_element} />
       <div className={styles.mobile_animation_carrousel_element} />
-    </div>
+    </motion.div>
   );
 };
 
 export const MobileAppAnimation: React.FC = () => {
   return (
-    <div className={styles.mobile_animation_mobile_container}>
-      <div className={styles.mobile_animation_notificationbar} />
-      <div className={styles.mobile_animation_row}>
-        <div className={styles.mobile_animation_banner} />
-      </div>
-      <div className={styles.mobile_animation_row}>
-        <div className={styles.mobile_animation_section_label} />
-        <Carrousel />
-      </div>
-      <div className={styles.mobile_animation_row}>
-        <div className={styles.mobile_animation_section_label} />
-        <Carrousel />
-      </div>
-      <div className={styles.mobile_animation_menubar}>
-        <div />
-        <div />
-        <div />
-      </div>
+    <div className={styles.mobile_animation_backdrop}>
+      <motion.div
+        className={styles.mobile_animation_mobile_container}
+        initial="hidden"
+        animate="visible"
+        variants={uiContainerVariants}
+      >
+        <motion.div
+          className={styles.mobile_animation_notificationbar}
+          variants={notificationsContainerVariants}
+        />
+        <div className={styles.mobile_animation_row}>
+          <motion.div
+            className={styles.mobile_animation_banner}
+            variants={uiFieldVariants}
+          />
+        </div>
+        <motion.div className={styles.mobile_animation_row}>
+          <motion.div
+            className={styles.mobile_animation_section_label}
+            variants={uiFieldVariants}
+          />
+          <Carrousel />
+        </motion.div>
+        <motion.div className={styles.mobile_animation_row}>
+          <motion.div
+            className={styles.mobile_animation_section_label}
+            variants={uiFieldVariants}
+          />
+          <Carrousel />
+        </motion.div>
+        <motion.div
+          className={styles.mobile_animation_menubar}
+          variants={navbarContainerVariants}
+        >
+          <div />
+          <div />
+          <div />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
--- src/modules/Services/ReactExpertice/ProductAnimation.tsx
+++ src/modules/Services/ReactExpertice/ProductAnimation.tsx
@@ -1,63 +1,172 @@
 import * as React from 'react';
+import {motion} from 'framer-motion';
 import * as styles from './ReactExpertice.module.scss';
+
+const uiContainerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      ease: 'linear',
+      duration: 0.25,
+      delay: 0.5,
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const uiFieldVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'afterChildren',
+      ease: 'linear',
+      duration: 0.25,
+    },
+  },
+};
+
+const gridVariants = {
+  test: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      ease: 'linear',
+      duration: 0.25,
+      delay: 2.35,
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const gridElementsVariants = {
+  test: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      when: 'afterChildren',
+      ease: 'linear',
+      duration: 0.25,
+    },
+  },
+};
 
 const GridElement: React.FC = () => {
   return (
-    <div className={styles.product_grid_element}>
+    <motion.div
+      className={styles.product_grid_element}
+      variants={gridElementsVariants}
+    >
       <div />
       <div />
       <div />
-    </div>
+    </motion.div>
   );
 };
 
 const SidebarFilter: React.FC = () => {
   return (
-    <div className={styles.product_sidebar_filter}>
+    <motion.div
+      className={styles.product_sidebar_filter}
+      variants={uiFieldVariants}
+    >
       <div />
       <div />
       <div />
       <div />
       <div />
-    </div>
+    </motion.div>
   );
 };
 
 export const ProductAnimation: React.FC = () => {
   return (
-    <div className={styles.product_container}>
-      <div className={styles.product_navbar}>
-        <div className={styles.product_navbar_button} />
-        <div className="flex">
-          <div className={styles.product_navbar_link} />
-          <div className={styles.product_navbar_link} />
-          <div className={styles.product_navbar_link} />
-          <div className={styles.product_navbar_link} />
-        </div>
-        <div className={styles.product_navbar_button} />
-      </div>
-      <div className={styles.product_actionbar}>
-        <div className={styles.product_actionbar_search} />
-        <div className="flex">
-          <div className={styles.product_actionbar_filter} />
-          <div className={styles.product_actionbar_filter} />
-          <div className={styles.product_actionbar_filter} />
-        </div>
-      </div>
-      <div className="flex justify-between">
-        <div className={styles.product_sidebar}>
-          <SidebarFilter />
-        </div>
-        <div className={styles.product_grid}>
-          <GridElement />
-          <GridElement />
-          <GridElement />
-          <GridElement />
-          <GridElement />
-          <GridElement />
-          <GridElement />
-        </div>
-      </div>
+    <div className={styles.product_backdrop}>
+      <motion.div
+        className={styles.product_container}
+        initial="hidden"
+        animate="visible"
+        variants={uiContainerVariants}
+      >
+        <motion.div className={styles.product_navbar}>
+          <motion.div
+            className={styles.product_navbar_button}
+            variants={uiFieldVariants}
+          />
+          <motion.div className="flex">
+            <motion.div
+              className={styles.product_navbar_link}
+              variants={uiFieldVariants}
+            />
+            <motion.div
+              className={styles.product_navbar_link}
+              variants={uiFieldVariants}
+            />
+            <motion.div
+              className={styles.product_navbar_link}
+              variants={uiFieldVariants}
+            />
+            <motion.div
+              className={styles.product_navbar_link}
+              variants={uiFieldVariants}
+            />
+          </motion.div>
+          <motion.div
+            className={styles.product_navbar_button}
+            variants={uiFieldVariants}
+          />
+        </motion.div>
+        <motion.div className={styles.product_actionbar}>
+          <motion.div
+            className={styles.product_actionbar_search}
+            variants={uiFieldVariants}
+          />
+          <motion.div className="flex">
+            <motion.div
+              className={styles.product_actionbar_filter}
+              variants={uiFieldVariants}
+            />
+            <motion.div
+              className={styles.product_actionbar_filter}
+              variants={uiFieldVariants}
+            />
+            <motion.div
+              className={styles.product_actionbar_filter}
+              variants={uiFieldVariants}
+            />
+          </motion.div>
+        </motion.div>
+        <motion.div className="flex justify-between">
+          <motion.div className={styles.product_sidebar}>
+            <SidebarFilter />
+          </motion.div>
+          <motion.div
+            className={styles.product_grid}
+            initial="test"
+            animate="show"
+            variants={gridVariants}
+          >
+            <GridElement />
+            <GridElement />
+            <GridElement />
+            <GridElement />
+            <GridElement />
+            <GridElement />
+            <GridElement />
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
