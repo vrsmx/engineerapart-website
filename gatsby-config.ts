20990ecfb89ff70b369eb492016b0bc7685ad9dd
vrsmx <vrsmx.master@gmail.com>
2023-01-24 17:18:57 -0600

    Icon update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -24,7 +24,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
+        icon: 'src/images/icon-gradient.png',
       },
     },
     'gatsby-plugin-sharp',
